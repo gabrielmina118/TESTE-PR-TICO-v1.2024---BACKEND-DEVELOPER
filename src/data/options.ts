5165ada020174386108ac1dfb42ba76bfcdbf016
gabriel Mina da Silva <gabrielmina118@gmail.com>
2024-05-30 17:41:15 -0300

    feat: :sparkles: dotenv

--- src/data/options.ts
+++ src/data/options.ts
@@ -2,14 +2,17 @@ import axios from "axios";
 import { IListAvailable } from "./interface/IListAvailable";
 import { IOrder } from "./interface/IOrder";
 import { OrderDTO } from "../controllers/OrderDTO";
+import { config } from "dotenv";
 
-const API_KEY = "ZtVdh8XQ2U8pWI2gmZ7f796Vh8GllXoN7mr0djNf";
+config();
+
+// const process.env.API_KEY = "ZtVdh8XQ2U8pWI2gmZ7f796Vh8GllXoN7mr0djNf";
 const BASE_URL = "https://api.tech.redventures.com.br";
 
 export const getAllBroths = async (): Promise<IListAvailable[]> => {
     try {
         const result = await axios.get<IListAvailable[]>(`${BASE_URL}/broths`, {
-            headers: { "x-api-key": API_KEY },
+            headers: { "x-api-key": process.env.API_KEY },
         });
         return result.data;
     } catch (error: any) {
@@ -26,7 +29,7 @@ export const getAllProteins = async (): Promise<IListAvailable[]> => {
         const result = await axios.get<IListAvailable[]>(
             `${BASE_URL}/proteins`,
             {
-                headers: { "x-api-key": API_KEY },
+                headers: { "x-api-key": process.env.API_KEY },
             }
         );
         return result.data;
@@ -41,27 +44,26 @@ export const getAllProteins = async (): Promise<IListAvailable[]> => {
 
 export const Order = async (body: OrderDTO): Promise<IOrder> => {
     try {
+        const allBroths = await getAllBroths();
+        const allProteins = await getAllProteins();
 
-       const allBroths = await getAllBroths();
-       const allProteins = await getAllProteins();
+        const validateBrothsId = allBroths.find(
+            (broth) => broth.id === body.brothId
+        );
 
-       const validateBrothsId = allBroths.find(
-           (broth) => broth.id === body.brothId
-       );
-       
-       if (!validateBrothsId) {
-           throw new Error("Invalid brothId");
-       }
+        if (!validateBrothsId) {
+            throw new Error("Invalid brothId");
+        }
 
-       const validateProteinsId = allProteins.find(
-           (protein) => protein.id === body.proteinId
-       );
-       if (!validateProteinsId) {
-           throw new Error("Invalid proteinId");
-       }
+        const validateProteinsId = allProteins.find(
+            (protein) => protein.id === body.proteinId
+        );
+        if (!validateProteinsId) {
+            throw new Error("Invalid proteinId");
+        }
 
         const result = await axios.post<IOrder>(`${BASE_URL}/orders`, body, {
-            headers: { "x-api-key": API_KEY },
+            headers: { "x-api-key": process.env.API_KEY },
         });
         return result.data;
     } catch (error: any) {
